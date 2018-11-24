db87a4992ead95fb75c3a0bcc450fea865cbb5ce
rockhopper72 <rockhopper72@users.noreply.github.com>
2018-11-24 10:38:16 -0500

    Add archive sites

--- resources/views/doc/index.blade.php
+++ resources/views/doc/index.blade.php
@@ -13,11 +13,10 @@
     <p>Compare our page with others:</p>
 
     <ul>
-        {{-- TODO --}}
-        <li><a href="#todo">Archive.org</a> (<a href="#todo">Save this webpage</a>)</li>
+        <li><a href="https://web.archive.org/web/https://notarizer.app/doc">Archive.org</a> (<a href="https://web.archive.org/save/https://notarizer.app/doc">Save this webpage</a>)</li>
         <li><a href="https://www.google.com/search?q=site%3Anotarizer.app%2Fdoc">Google Cache</a> (Click the dropdown next to the website URL, then click "Cached")</li>
         <li><a href="https://bing.com/search?q=site%3Anotarizer.app%2Fdoc">Bing Cache</a> (Click the dropdown next to the website URL, then click "Cached")</li>
-        <li><a href="#todo">archive.is</a></li>
+        <li><a href="https://archive.fo/https://notarizer.app/doc">archive.is</a></li>
         <li>Have an auditing service? <a href="{{ route('contact.create', ['subject' => 'I have an auditing service']) }}">Contact us to add it!</a></li>
     </ul>
 
