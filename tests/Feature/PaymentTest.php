c4919f75bfad4a1e13df1c24911181d91d672d8a
rockhopper72 <rockhopper72@users.noreply.github.com>
2019-03-09 08:23:34 -0500

    Test payments

--- tests/Feature/PaymentTest.php
+++ tests/Feature/PaymentTest.php
@@ -2,19 +2,16 @@
 
 namespace Tests\Feature;
 
+use App\Payments\Payments;
+use Stripe\Error\Api;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PaymentTest extends TestCase
 {
-    // TODO:
-    // - It shows the payment page
-    // - The payment page can be submitted
-    // Test different sources: https://stripe.com/docs/testing
-    // It shows whats wrong when something happens
-    // It requires valid inputs
-    // It shows a message and redirects on success
     use RefreshDatabase;
+
+    // TODO: Test the \App\Payments\StripePayments class
 
     /** @test */
     public function it_shows_the_payment_page()
@@ -23,5 +20,95 @@ class PaymentTest extends TestCase
 
         $response->assertSee('Payment');
         $response->assertOk();
+    }
+
+    /** @test */
+    public function it_can_be_submitted()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->mock(Payments::class, function($mock) {
+            $mock->shouldReceive()
+                ->charge(20000, 'tok_visa', 'test@email.com', null)
+                ->once();
+        });
+
+        $response = $this->post('/payment', [
+            'amount' => '200', // $200
+            'stripeToken' => 'tok_visa', // See: https://stripe.com/docs/testing
+            'email' => 'test@email.com'
+        ]);
+
+        $response->assertRedirect('/');
+
+        $response->assertSessionHas('payment_confirmation');
+    }
+
+    /** @test */
+    public function it_requires_valid_input()
+    {
+        $this->mock(Payments::class, function($mock) {
+            $mock->shouldNotReceive()
+                ->charge(20000, 'tok_visa', 'test@email.com', null);
+        });
+
+        $response = $this->post('/payment', [
+            'amount' => 'abc', // $200
+            'stripeToken' => null, // See: https://stripe.com/docs/testing
+            'email' => 'not_an_email'
+        ]);
+
+        $response->assertSessionHasErrors(['amount', 'stripeToken', 'email']);
+    }
+    
+    /** @test */
+    public function it_redirects_to_for_document()
+    {
+        $this->mockPayments();
+
+        $response = $this->post('/payment', [
+            'amount' => '200', // $200
+            'stripeToken' => 'tok_visa', // See: https://stripe.com/docs/testing
+            'email' => 'test@email.com',
+            'for' => '2xdozNp7fnVbNP4HAmlpv8QRoE06cKLY6vJOKHuTbjIXT1P5go9k0GtWn7MW0oxM'
+        ]);
+
+        $response->assertRedirect('/doc/2xdozNp7fnVbNP4HAmlpv8QRoE06cKLY6vJOKHuTbjIXT1P5go9k0GtWn7MW0oxM');
+
+        $response->assertSessionHas('payment_confirmation');
+    }
+    
+    /** @test */
+    public function it_shows_when_there_is_a_problem()
+    {
+        $this->mock(Payments::class, function($mock) {
+            $mock->shouldReceive('charge')
+                ->andThrow(Api::class);
+        });
+
+        $response = $this->post('/payment', [
+            'amount' => '200', // $200
+            'stripeToken' => 'tok_visa', // See: https://stripe.com/docs/testing
+            'email' => 'test@email.com',
+        ]);
+
+        $response->assertSessionHas('payment_error');
+
+
+        $response2 = $this->followingRedirects()->post('/payment', [
+            'amount' => '200', // $200
+            'stripeToken' => 'tok_visa', // See: https://stripe.com/docs/testing
+            'email' => 'test@email.com',
+        ]);
+
+        $response2->assertSeeText('Your payment of $200.00 failed!');
+    }
+
+
+    private function mockPayments()
+    {
+        $this->mock(Payments::class, function($mock) {
+            $mock->shouldReceive('charge');
+        });
     }
 }
