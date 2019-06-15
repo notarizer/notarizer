4b3f134967c6477a7b947e9f2af2c920f60440a7
rockhopper72 <github2@steven.globaloffset.com>
2019-06-15 17:21:15 -0400

    Remove phone number

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -5,7 +5,5 @@
 @section('content')
     <h1>Contact Us</h1>
 
-    <p class="mb-2">Phone: <strong><a href="tel:18776176458">+1 (877) 617-6458</a></strong></p>
-
     <p class="mb-2">Email: <strong><a href="mailto:hello@notarizer.app">hello@notarizer.app</a></strong></p>
 @endsection
